5d57ed3180e3c9b0af45fe213e39729f88b7f8a3
Kirill <kirill2494@mail.ru>
2023-04-14 13:30:15 +0400

    add checkbox

--- src/components/appAccounts/AppAccounts.tsx
+++ src/components/appAccounts/AppAccounts.tsx
@@ -45,7 +45,12 @@ const AppAccounts = () => {
               <th>Обновить все</th>
             </tr>
             <tr className="accounts-table__account">
-              <td>7d625ba1-7640-441f-9ab7-e7dc6c24a085</td>
+              <td>
+                <label className="accounts-table__account-label">
+                  <input className="accounts-table__account-checkbox" type="checkbox" name="select-account" value="1" />
+                  7d625ba1-7640-441f-9ab7-e7dc6c24a085
+                </label>
+              </td>
               <td>09.04.2022 14:52</td>
               <td>1022</td>
               <td>
@@ -60,7 +65,12 @@ const AppAccounts = () => {
               </td>
             </tr>
             <tr className="accounts-table__account">
-              <td>7d625ba1-7640-441f-9ab7-e7dc6c24a085</td>
+              <td>
+                <label className="accounts-table__account-label">
+                  <input className="accounts-table__account-checkbox" type="checkbox" name="select-account" value="1" />
+                  7d625ba1-7640-441f-9ab7-e7dc6c24a085
+                </label>
+              </td>
               <td>09.04.2022 14:52</td>
               <td>1022</td>
               <td>
@@ -75,7 +85,12 @@ const AppAccounts = () => {
               </td>
             </tr>
             <tr className="accounts-table__account">
-              <td>7d625ba1-7640-441f-9ab7-e7dc6c24a085</td>
+              <td>
+                <label className="accounts-table__account-label">
+                  <input className="accounts-table__account-checkbox" type="checkbox" name="select-account" value="1" />
+                  7d625ba1-7640-441f-9ab7-e7dc6c24a085
+                </label>
+              </td>
               <td>09.04.2022 14:52</td>
               <td>1022</td>
               <td>
