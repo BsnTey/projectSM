7341d9ab015c90eaa5822e7c2bc68130f31208a9
Kirill <kirill2494@mail.ru>
2023-04-14 13:03:55 +0400

    complete list page html css

--- src/components/appAccounts/AppAccounts.tsx
+++ src/components/appAccounts/AppAccounts.tsx
@@ -2,28 +2,94 @@ import "./appAccounts.scss";
 
 const AppAccounts = () => {
   return (
-    <div className="app-accounts container">
-      <h1 className="app-title app-accounts__title">Список аккаунтов</h1>
-      <div className="accounts-section app-accounts__section">
-        <div className="panel-tabs accounts-section__tabs">
-          <nav className="panel-tabs__items">
-            <a href="#tab_01" className="panel-tabs__item">
-              Поиск
-            </a>
-            <a href="#tab_02" className="panel-tabs__item">
-              Cookie
-            </a>
-          </nav>
-          <div className="panel-tabs_body">
-            <div id="tab_01" className="panel-tabs__block">
-              Поисковая вкладка. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, aperiam repellendus? Deserunt doloribus culpa maxime iusto corrupti veniam,
-              optio, ducimus incidunt obcaecati, nobis esse magni minus. Pariatur error consectetur ab?
-            </div>
-            <div id="tab_02" className="panel-tabs__block">
-              Cookie вкладка. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit deserunt quia, rem, nesciunt consectetur a mollitia quod eaque iusto numquam aliquid
-              reiciendis recusandae vitae quam quisquam cumque voluptatum porro praesentium?
-            </div>
+    <div className="app-body container">
+      <h1 className="app-title app-body__title">Список аккаунтов</h1>
+      <div className="app-body__section">
+        <nav className="app-menu">
+          <a className="link-home app-menu__item">Личный кабинет</a>
+          <a className="link-list app-menu__item">Список</a>
+        </nav>
+
+        <div className="search-body">
+          <div className="filter-btns search-body__btns">
+            <form className="filter-form filter-btns__input-form" action="">
+              <label className="filter-form__label">
+                <input className="filter-form__input" placeholder="Искать" type="text" name="search" />
+              </label>
+              <button className="filter-form__button" type="button" />
+            </form>
+
+            <button className="filter-btn filter-btns__down">
+              <span>По убыванию</span>
+            </button>
+
+            <button className="filter-btn filter-btns__up">
+              <span>По возрастанию</span>
+            </button>
+
+            <button className="filter-btn filter-btns__add">
+              <span>Добавить</span>
+            </button>
+
+            <button className="filter-btn filter-btns__update">
+              <span>Обновить все</span>
+            </button>
           </div>
+
+          <table className="accounts-table search-body__block-accounts">
+            <tr className="accounts-table__title">
+              <th>Token</th>
+              <th>Обновлено</th>
+              <th>Баланс</th>
+              <th>Получить Cookie</th>
+              <th>Обновить все</th>
+            </tr>
+            <tr className="accounts-table__account">
+              <td>7d625ba1-7640-441f-9ab7-e7dc6c24a085</td>
+              <td>09.04.2022 14:52</td>
+              <td>1022</td>
+              <td>
+                <button className="account-btn">
+                  <span className="accounts-table__btn-key"></span>
+                </button>
+              </td>
+              <td>
+                <button className="account-btn">
+                  <span className="accounts-table__btn-update"></span>
+                </button>
+              </td>
+            </tr>
+            <tr className="accounts-table__account">
+              <td>7d625ba1-7640-441f-9ab7-e7dc6c24a085</td>
+              <td>09.04.2022 14:52</td>
+              <td>1022</td>
+              <td>
+                <button className="account-btn">
+                  <span className="accounts-table__btn-key"></span>
+                </button>
+              </td>
+              <td>
+                <button className="account-btn">
+                  <span className="accounts-table__btn-update"></span>
+                </button>
+              </td>
+            </tr>
+            <tr className="accounts-table__account">
+              <td>7d625ba1-7640-441f-9ab7-e7dc6c24a085</td>
+              <td>09.04.2022 14:52</td>
+              <td>1022</td>
+              <td>
+                <button className="account-btn">
+                  <span className="accounts-table__btn-key"></span>
+                </button>
+              </td>
+              <td>
+                <button className="account-btn">
+                  <span className="accounts-table__btn-update"></span>
+                </button>
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
