7f5a5d7c179d0b139fcf5ca9d642e74abd1314b4
Kirill <kirill2494@mail.ru>
2023-04-18 22:17:31 +0400

    split header, removed header-bar, headerSearch

--- src/components/headerBar/HeaderBar.tsx
+++ src/components/headerBar/HeaderBar.tsx
@@ -1,5 +1,12 @@
+import "./headerBar.scss";
+
 const HeaderBar = () => {
-  return <></>;
+  return (
+    <div className="header-bar container">
+      <div className="header-bar__city">Москва</div>
+      <div className="header-bar__phone">8 800 777-777-1</div>
+    </div>
+  );
 };
 
 export default HeaderBar;
