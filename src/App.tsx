60548ed5e80c0c04612f3931e54f8c46130cefa3
Kirill <kirill2494@mail.ru>
2023-04-18 13:44:28 +0400

    small decorate code

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/header/Header";
-
 import AppAccounts from "./pages/appAccounts/AppAccounts";
-import AddAccount from "./components/addAccount/AddAccount";
 
 const App = () => {
   return (
