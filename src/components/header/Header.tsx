c3b6c5f4f3a082e6d66ac488b0e23ff1a4c99576
Kirill <kirill2494@mail.ru>
2023-04-20 15:34:06 +0400

    branch 13 merge master

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,24 +1,14 @@
+import HeaderBar from "../headerBar/HeaderBar";
+import HeaderSearch from "../headerSearch/HeaderSearch";
 import "./header.scss";
 
 const Header = () => {
   return (
     <>
-      <div className="header-bar container">
-        <div className="header-bar__city">Москва</div>
-        <div className="header-bar__phone">8 800 777-777-1</div>
-      </div>
+      <HeaderBar />
       <div className="header container">
         <a className="header__logo" href="/"></a>
-        <div className="sm-search-wrapper">
-          <form className="header__input-search sm-search" action="" method="get">
-            <label className="sm-search__input-wpapper">
-              <input className="sm-search__input" placeholder="Поиск" type="text" name="search" />
-            </label>
-            <button className="sm-search__btn" type="button">
-              Найти
-            </button>
-          </form>
-        </div>
+        <HeaderSearch />
         <button className="header__login">Войти</button>
       </div>
     </>
