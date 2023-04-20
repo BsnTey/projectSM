ee32e6c9717f111fb051a6a2e19668e9449e8894
Kirill <kirill2494@mail.ru>
2023-04-20 18:58:18 +0400

    delete translationTitles

--- src/components/tableHeader/TableHeader.tsx
+++ src/components/tableHeader/TableHeader.tsx
@@ -2,21 +2,8 @@ import { useSelector } from "react-redux";
 import { titleSelector } from "../../store/accountSlice";
 import "./tableHeader.scss";
 
-const translationTitles = (titles: string[]) => {
-  const translation: Record<string, string> = {
-    token: "Токен",
-    dateCheck: "Обновлено",
-    amount: "Баланс",
-  };
-  const newTitles = titles.map((title) => translation[title] ?? title);
-  newTitles.push("Получить Cookie", "Обновить все");
-
-  return newTitles;
-};
-
 const TableHeader = () => {
   let titles = useSelector(titleSelector);
-  titles = translationTitles(titles);
 
   return (
     <tr>
