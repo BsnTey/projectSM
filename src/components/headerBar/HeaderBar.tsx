0b2008d29b1c13e7c8a61be6cb5232b8de88db7f
Kirill <kirill2494@mail.ru>
2023-04-21 14:43:54 +0400

    added a language change mechanism. updated translation en

--- src/components/headerBar/HeaderBar.tsx
+++ src/components/headerBar/HeaderBar.tsx
@@ -4,10 +4,18 @@ import "./headerBar.scss";
 const HeaderBar = () => {
   const { t, i18n } = useTranslation();
 
+  const changeLanguage = (language: string) => {
+    i18n.changeLanguage(language);
+  };
+
   return (
     <div className="header-bar container">
       <div className="header-bar__city">{t("city.moscow")}</div>
       <div className="header-bar__phone">8 800 777-777-1</div>
+      <div className="header-bar__lang">
+        {t("language")}:<button onClick={() => changeLanguage("ru")}>RU</button>
+        <button onClick={() => changeLanguage("en")}>EN</button>
+      </div>
     </div>
   );
 };
