6706ba36ab9ea1b21cea7126084f92835c9e3a25
Kirill <kirill2494@mail.ru>
2023-04-21 19:24:58 +0400

    change userSlice

--- src/store/userSlice.tsx
+++ src/store/userSlice.tsx
@@ -1,11 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface User {
+  id: number;
+  token: string;
+  email: string;
+  loggedIn: boolean;
+}
+
+const initialState: User = {
+  id: 0,
+  token: "",
+  email: "",
+  loggedIn: false,
+};
+
 export const userSlice = createSlice({
   name: "user",
-  initialState: {
-    email: "",
-    loggedIn: false,
-  },
+  initialState,
   reducers: {
     setEmail(state, action) {
       state.email = action.payload;
