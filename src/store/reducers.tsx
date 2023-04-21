263e966f4a0c59a2b0f720265858e13efddc94a0
Kirill <kirill2494@mail.ru>
2023-04-21 12:55:35 +0400

    add reg userSlice

--- src/store/reducers.tsx
+++ src/store/reducers.tsx
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import accountSlice from "./accountSlice";
+import userSlice from "./userSlice";
 
 const rootReducer = combineReducers({
   accounts: accountSlice,
+  users: userSlice,
 });
 
 export default rootReducer;
